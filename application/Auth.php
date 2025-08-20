a9fe7367be7db3bc5a60aa3c9ce34079fe06e907
gt116611 <2292222999@qq.com>
2025-08-21 03:43:25 +0800

    Add files via upload

---
+++ application/Auth.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'app_version' => '1.1',
+    'app_name' => '水果机授权',
+    'app_author' => '浅月流歌',
+    'app_qq' => '1817181911',
+    'app_url' => 'www.91ym.top',
+    'authcode' => '37dfb34583d38d21b974cf506c8a3c88',
+];
+?>
